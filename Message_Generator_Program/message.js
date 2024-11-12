690e423736bc6b500317c0de1539d1f8acbe5e53
Anna-Trang Nguyen <159499874+atn6658@users.noreply.github.com>
2024-11-12 11:34:23 -0600

    Added Message Components

---
+++ Message_Generator_Program/message.js
@@ -0,0 +1,20 @@
+function generateRandomNumber(num) {
+    return Math.floor(Math.random() * num);
+}
+
+const whatToDoToday = {
+
+    mood: ['Happy', 'Tired', 'Sad', 'Indifferent', 'Bored'],
+    activites: ['Take a Rest Day', 'Study for 3 hours', 'Start/Finish Chores', 'Play Games','Watch a movie/TV show'],
+    food: ['Chipotle', 'Wing Stop', 'Cook at Home', 'Panda Express', 'K-town Food', 'Sushi', 'Tacos']
+}
+
+let choiceOfTheDay = []; 
+
+
+
+
+ 
+   
+
+    
