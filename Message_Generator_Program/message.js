98699be23192b8c2d18a23f5831aecfff3b5d1a2
Anna-Trang Nguyen <159499874+atn6658@users.noreply.github.com>
2024-11-12 11:52:45 -0600

    debugged whatToDoToday

--- Message_Generator_Program/message.js
+++ Message_Generator_Program/message.js
@@ -3,37 +3,42 @@ function generateRandomNumber(num) {
 }
 
 const whatToDoToday = {
-
     mood: ['Happy', 'Tired', 'Sad', 'Indifferent', 'Bored'],
-    activites: ['Take a Rest Day', 'Study for 3 hours', 'Start/Finish Chores', 'Play Games','Watch a movie/TV show'],
+    activities: ['Take a Rest Day', 'Study for 3 hours', 'Start/Finish Chores', 'Play Games', 'Watch a movie/TV show'],
     food: ['Chipotle', 'Wing Stop', 'Home food', 'Panda Express', 'K-town Food', 'Sushi', 'Tacos']
-}
+};
 
-let choiceOfTheDay = []; 
+let choiceOfTheDay = [];
 
-for (let options of whatToDoToday) { 
-    let theChoice = generateRandomNumber(options.length)
 
-    switch(options) {
+for (let option in whatToDoToday) {
+
+    let theChoice = generateRandomNumber(whatToDoToday[option].length);
+
+  
+    switch(option) {
         case 'mood':
-            choiceOfTheDay.push(`Your mood today is ${mood[options][theChoice]}.`)
-        case 'activites':
-            choiceOfTheDay.push(`Today you should ${activites[options][theChoice]}`)
+            choiceOfTheDay.push(`Your mood today is ${whatToDoToday[option][theChoice]}.`);
+            break;
+        case 'activities':
+            choiceOfTheDay.push(`Today you should ${whatToDoToday[option][theChoice]}.`);
+            break;
         case 'food':
-            choiceOfTheDay.push(`Here is what I recommend you to eat today ${food[options][theChoice]}`)
-        default: 
-            choiceOfTheDay.push('You did not enter anything in you silly bean') 
-        
+            choiceOfTheDay.push(`Here is what I recommend you eat today: ${whatToDoToday[option][theChoice]}.`);
+            break;
+        default:
+            choiceOfTheDay.push('You did not enter anything, you silly bean!');
+            break;
     }
 }
 
-function formatChoice(choice) {
- 
-    const formatted = choiceOfTheDay.join('\n')
-    console.log(formatted)
-  }
-  
-  formatChoice(choiceOfTheDay);
+function formatChoice(choices) {
+    const formatted = choices.join('\n');
+    console.log(formatted);
+}
+
+formatChoice(choiceOfTheDay);
+
 
 
  
